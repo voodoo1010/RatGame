3480de1303cf20ae7ab358dd8c6ed4f093cd4da6
usama.riaz <usama.riaz.1010@outlook.com>
2026-02-12 23:59:54 +0900

    switch from urp to hdrp

--- Assets/_Features/Tablet/Scripts/TabletDisplay/TabletDisplay.cs
+++ Assets/_Features/Tablet/Scripts/TabletDisplay/TabletDisplay.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class TabletDisplay : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int _firstInt;
+    [SerializeField] private int secondInt;
+    public int ThirdInt;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+
     }
 }
